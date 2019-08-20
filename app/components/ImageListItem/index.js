120e13a4b396f3b58889f148db4c882c69fd863d
bradywatkinson <brady.a.watkinson@gmail.com>
2019-08-20 14:55:13 +1000

    Style ImageListItem

--- app/components/ImageListItem/index.js
+++ app/components/ImageListItem/index.js
@@ -9,9 +9,19 @@ import { Link } from 'react-router-dom';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForwardIos';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  listText: {
+    'white-space': 'nowrap',
+    overflow: 'hidden',
+    'text-overflow': 'ellipsis',
+  },
+});
 
 function ImageListItem({ data, index, style }) {
   const image = data[index];
+  const classes = useStyles();
 
   return (
     <ListItem
@@ -21,7 +31,12 @@ function ImageListItem({ data, index, style }) {
       style={style}
       key={index}
     >
-      <ListItemText primary={image.title} />
+      <ListItemText
+        primary={image.title || <i>(untitled)</i>}
+        secondary={image.author}
+        classes={{ primary: classes.listText, secondary: classes.listText }}
+        primaryTypographyProps={{ component: 'p' }}
+      />
       <ArrowForwardIcon />
     </ListItem>
   );
